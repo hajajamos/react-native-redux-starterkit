52081f269f791154d2147e5a38f7f4024212c7c0
Hajaj <hajajamos@gmail.com>
2017-12-26 17:22:08 +0200

    Added tests to HomeScreen.Test.js

--- __tests__/components/HomeScreen.tests.js
+++ __tests__/components/HomeScreen.tests.js
@@ -3,9 +3,51 @@ import * as Jest from "jest";
 import React from "react";
 import HomeScreen from "../../components/HomeScreen/HomeScreen"
 import * as enzyme from "enzyme";
+import { Title } from 'native-base';
+
+function init() {
+	const props = {
+		openDrawer: jest.fn()
+	}
+
+	const wrapper = enzyme.shallow(<HomeScreen {...props} />);
+	return { wrapper, props };
+}
+
 
 describe("<HomeScreen/>", () => {
 	it("should contains 'title' with HomeScreen", () => {
-		expect(true).toBe(true);
+		//Arrange
+		const { wrapper } = init();
+		const title = wrapper.find(Title).props().children;
+
+		//Assert
+		expect(title).toBe("HomeScreen");
+	});
+
+	it("button 'openDrawer' click will call to ", () => {
+		//Arrange
+		const { wrapper,props } = init();
+		const openDrawer = wrapper.find("#openDrawer");
+		expect(props.openDrawer).toHaveBeenCalledTimes(0);
+
+		//Act
+		openDrawer.simulate("press");
+		
+		//Assert
+		expect(props.openDrawer).toHaveBeenCalledTimes(1);
+	});
+
+	it("button 'openDrawer2' click will call to ", () => {
+		//Arrange
+		const { wrapper,props } = init();
+		const openDrawer = wrapper.find("#openDrawer2");
+		expect(props.openDrawer).toHaveBeenCalledTimes(0);
+
+		//Act
+		openDrawer.simulate("press");
+		
+		//Assert
+		expect(props.openDrawer).toHaveBeenCalledTimes(1);
 	});
 });
