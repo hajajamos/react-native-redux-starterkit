6084d40315cafbec9ef84b9c276bb03a54ca2a92
Hajaj <hajajamos@gmail.com>
2017-12-26 10:11:13 +0200

    Added temp passing test file (assert true === true)

---
+++ __tests__/components/HomeScreen.tests.js
@@ -0,0 +1,11 @@
+import "react-native";
+import * as Jest from "jest";
+import React from "react";
+import HomeScreen from "../../components/HomeScreen/HomeScreen"
+import * as enzyme from "enzyme";
+
+describe("<HomeScreen/>", () => {
+	it("should contains 'title' with HomeScreen", () => {
+		expect(true).toBe(true);
+	});
+});
