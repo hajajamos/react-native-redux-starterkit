8e890b08844335ed9e1039bacd4ef66de176a256
Hajaj <hajajamos@gmail.com>
2017-12-30 15:50:25 +0200

    re-write the index.js

--- index.js
+++ index.js
@@ -1,27 +1,15 @@
 import { AppRegistry } from "react-native";
 import React from "react";
-import HomeScreen from "./components/HomeScreen/HomeScreenPage";
-import SideBar from "./components/SideBar/SideBar";
-import { DrawerNavigator } from "react-navigation";
 import { Provider } from 'react-redux';
 import getStore from './redux/store/configureStore';
-import TestReduxPage from './components/TestRedux/TestReduxPage';
-
-const routes = {
-	"Home": { screen: HomeScreen },
-	"TestRedux": { screen: TestReduxPage },
-};
-
-const HomeScreenRouter = DrawerNavigator(routes, {
-	contentComponent: navProps => 
-	(<SideBar navigation={navProps.navigation} routes={Object.keys(routes)} />)
-});
+import ReduxNavigation from './components/ReduxNavigation/ReduxNavigation';
 
 const store = getStore();
+
 const test = () => {
 	return(
 		<Provider store={store}>
-				<HomeScreenRouter/>
+				<ReduxNavigation/>
 		</Provider>
 	);
 };
