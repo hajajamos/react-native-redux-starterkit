a7300e7ba7148089ddf86d90879f64e96f8135d7
Hajaj <hajajamos@gmail.com>
2017-12-30 15:50:14 +0200

    Re-write the app navigation

--- components/ReduxNavigation/AppNavigation.js
+++ components/ReduxNavigation/AppNavigation.js
@@ -1,17 +1,41 @@
-import { DrawerNavigator } from 'react-navigation';
+import React from "react";
+import { DrawerNavigator, StackNavigator } from "react-navigation";
+import { Animated, Easing } from "react-native";
 import HomeScreen from "../HomeScreen/HomeScreenPage";
-import TestRedux from '../TestRedux/TestRedux';
+import SideBar from "../SideBar/SideBar";
+import TestReduxPage from "./../TestRedux/TestReduxPage";
 
+const noTransitionConfig = () => ({
+	transitionSpec: {
+		duration: 0,
+		timing: Animated.timing,
+		easing: Easing.step0
+	}
+});
 
+const drawerStackRoutes = {
+	"HomeScreen": { screen: HomeScreen },
+	"TestReduxPage": { screen: TestReduxPage },
+};
+
+const DrawerStack = DrawerNavigator(
+	drawerStackRoutes, {
+		gesturesEnabled: true,
+		contentComponent: (navProps) => (<SideBar navigation={navProps.navigation} routes={Object.keys(drawerStackRoutes)}/>),
+		drawerOpenRoute: "DrawerOpen",
+		drawerCloseRoute: "DrawerClose",
+		drawerToggleRoute: "DrawerToggle"
+	});
 
 // drawer stack
-const PrimaryNav = DrawerNavigator({
-	HomeScreen: { screen: HomeScreen },
-	TestRedux: { screen: TestRedux },
+const PrimaryNav = StackNavigator({
+	drawerStack: { screen: DrawerStack }
 }, {
-	gesturesEnabled: true,
-	// contentComponent: SideMenu,
-	headerMode: 'screen',
+	// Default config for all screens
+	headerMode: "none",
+	title: "Main",
+	initialRouteName: "drawerStack",
+	transitionConfig: noTransitionConfig,
 });
 
 export default PrimaryNav;
