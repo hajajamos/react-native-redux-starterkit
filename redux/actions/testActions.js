2463fcf058350a3123adff6e1f59efd51a4c0d28
Hajaj <hajajamos@gmail.com>
2018-01-03 21:43:02 +0200

    Separate the addNumberToState and addNumberToStateSuccess to two functions

--- redux/actions/testActions.js
+++ redux/actions/testActions.js
@@ -1,12 +1,16 @@
 import * as actionsTypes from "./actionsTypes";
 
+export function addNumberToStateSuccess(numberToAdd) {
+	return {
+		type: actionsTypes.ADD_NUMBER_TO_STATE_SUCCESS,
+		numberToAdd: numberToAdd
+	};
+}
+
 export function addNumberToState(numberToAdd) {
 	return(dispatch) => {
 		// thunks allow for pre-processing actions, calling apis, and dispatching multiple actions
 		// in this case at this point we could call a service that would persist the fuel savings
-		return dispatch({
-			type: actionsTypes.ADD_NUMBER_TO_STATE_SUCCESS,
-			numberToAdd: numberToAdd
-		});
+		return dispatch(addNumberToStateSuccess(numberToAdd));
 	};
 }
